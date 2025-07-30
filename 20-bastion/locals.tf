4c4de0e77fe410d671fc8f2f8ccd4956c26c06a6
b27user12 <b27user12@rpsconsulting.in>
2025-07-31 01:54:40 +0530

    hey

--- 20-bastion/locals.tf
+++ 20-bastion/locals.tf
@@ -1,6 +1,6 @@
 locals {
   ami_id=data.aws_ami.myami.id
-  bastion_sg_id=local.aws_ssm_parameter.bastion_sg_id.value
+  bastion_sg_id=data.aws_ssm_parameter.bastion_sg_id.value
   public_subnet_ids=split(",",data.aws_ssm_parameter.public_subnet_ids.value)[0]
 
     common_tags = {
