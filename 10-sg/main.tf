910c54b1a26f647d71fc4913c624d6490b549ec4
b27user12 <b27user12@rpsconsulting.in>
2025-07-31 01:20:27 +0530

    updatedbastion

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -8,3 +8,24 @@ module "frontend" {
     vpc_id=local.vpc_id
   
 }
+
+module "bastion" {
+    # source = "../../terraform-aws-sg"
+    source = "git::https://github.com/Sameer-Sarrainodu/Terraform-Aws-Sg.git?ref=main"
+    project = var.project
+    environment = var.environment
+    sg_description = "sg for bastion"
+    sg_name= "bastion"
+    vpc_id=local.vpc_id
+  
+}
+
+resource "aws_security_group_rule" "bastion_laptop" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.bastion.sg_id
+}
+
---
+++ 20-bastion/main.tf
@@ -0,0 +1,15 @@
+resource "aws_instance" "web" {
+  ami           = local.ami_id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [local.bastion_sg_id]
+  subnet_id = local.public_subnet_ids
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-bastion"
+    }
+  )
+     
+  
+}
