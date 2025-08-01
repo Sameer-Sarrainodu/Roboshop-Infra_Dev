9c934a2daa78716286a84ba1506e58e5ec164248
b27user12 <b27user12@rpsconsulting.in>
2025-08-01 14:02:09 +0530

    hey

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -29,3 +29,23 @@ resource "aws_security_group_rule" "bastion_laptop" {
   security_group_id = module.bastion.sg_id
 }
 
+
+module "backend_alb" {
+    # source = "../../terraform-aws-sg"
+    source = "git::https://github.com/Sameer-Sarrainodu/Terraform-Aws-Sg.git?ref=main"
+    project = var.project
+    environment = var.environment
+    sg_description = "sg for backend_alb"
+    sg_name= "backend_alb"
+    vpc_id=local.vpc_id
+  
+}
+# backend ALB accepting connections from my bastion host on port no 80
+resource "aws_security_group_rule" "backend_alb_bastion" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  source_security_group_id = module.bastion.sg_id
+  security_group_id = module.backend_alb.sg_id
+} 
