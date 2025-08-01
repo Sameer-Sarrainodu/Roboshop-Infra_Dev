2e782a7fbd7b644133a7a53315c915dd33b20999
b27user12 <b27user12@rpsconsulting.in>
2025-08-02 02:05:54 +0530

    sg

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -93,3 +93,11 @@ resource "aws_security_group_rule" "vpn_943" {
   cidr_blocks = ["0.0.0.0/0"]
   security_group_id = module.vpn.sg_id
 } 
+resource "aws_security_group_rule" "backend_alb_vpn" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  source_security_group_id = module.vpn.sg_id
+  security_group_id = module.backend_alb.sg_id
+}
