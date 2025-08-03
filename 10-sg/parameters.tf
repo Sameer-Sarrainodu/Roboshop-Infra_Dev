403a6ca8d55bed0b0b3614467c40f196585f6d5b
b27user12 <b27user12@rpsconsulting.in>
2025-08-03 23:55:49 +0530

    hi

--- 10-sg/parameters.tf
+++ 10-sg/parameters.tf
@@ -14,7 +14,7 @@ resource "aws_ssm_parameter" "backend_alb_sg_id" {
   type  = "String"
   value = module.backend_alb.sg_id
 }
-resource "aws_ssm_parameter" "vpn" {
+resource "aws_ssm_parameter" "vpn_sg_id" {
   name  = "/${var.project}-${var.environment}/vpn"
   type  = "String"
   value = module.vpn.sg_id
