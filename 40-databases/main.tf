68caf51b232369a172e7dc0c4902f65ec683b773
b27user12 <b27user12@rpsconsulting.in>
2025-08-04 20:14:00 +0530

    added iamssm param

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -95,6 +95,7 @@ resource "aws_instance" "mysql" {
   instance_type = "t3.micro"
   vpc_security_group_ids = [local.mysql_sg_id]
   subnet_id = local.database_subnet_id
+  iam_instance_profile = "ec2roleforssmparameters"
   tags = merge(
     local.common_tags,
     {
