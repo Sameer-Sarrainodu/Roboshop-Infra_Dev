45f1b8434ee42c597a69e45702acf1bf3dfd5fca
b27user12 <b27user12@rpsconsulting.in>
2025-08-20 17:33:47 +0530

    yup

--- 50-backend_alb/parameters.tf
+++ 50-backend_alb/parameters.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "backend_alb_listner_arn" {
-    name  = "/${var.project}-${var.environment}/backend_alb_arn"
+    name  = "/${var.project}-${var.environment}/backend_alb_listner_arn"
     type  = "String"
     value = module.backend_alb.arn
 }
