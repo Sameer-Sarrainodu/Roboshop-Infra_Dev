36bdaee7f45b15433dbb3b3573bb1b4be901e306
b27user12 <b27user12@rpsconsulting.in>
2025-08-20 16:25:54 +0530

    yup

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -2,4 +2,4 @@
 
 component=$1
 dnf install ansible -y
-ansible-pull -U https://github.com/Sameer-Sarrainodu/Ansible-Roboshop-Roles.git -e component=$1 -e env=$2 main.yaml
+ansible-pull -U https://github.com/Sameer-Sarrainodu/Ansible-Roles-Terraform.git -e component=$1 -e env=$2 main.yaml
