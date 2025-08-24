a19ad04a8575b1ef9bd9e3fa9743fbfe29349767
b27user12 <b27user12@rpsconsulting.in>
2025-08-25 01:24:10 +0530

    added cdn

---
+++ 91-cdn/variables.tf
@@ -0,0 +1,19 @@
+variable "project" {
+    default = "roboshop"
+  
+}
+variable "environment" {
+    default = "dev"
+  
+}
+variable "zone_name" {
+    type=string
+    default = "sharkdev.shop"
+  
+}
+
+
+variable "zone_id" {
+    default = "Z0022572U6LHZ3ASAGBB"
+  
+}
