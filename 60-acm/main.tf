d96aa74b208ba4083710debbd26c6f8a40ea3b4b
b27user12 <b27user12@rpsconsulting.in>
2025-08-25 10:32:11 +0530

    a error fix in acm

--- 60-acm/main.tf
+++ 60-acm/main.tf
@@ -16,7 +16,7 @@ resource "aws_acm_certificate" "sharkdev" {
 
 resource "aws_route53_record" "sharkdev" {
   for_each = {
-    for dvo in aws_acm_certificate.example.domain_validation_options : dvo.domain_name => {
+    for dvo in aws_acm_certificate.sharkdev.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
       type   = dvo.resource_record_type
