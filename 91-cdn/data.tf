cf4bafe032057375e421c9c66c46740ae313cf37
b27user12 <b27user12@rpsconsulting.in>
2025-08-26 15:42:24 +0530

    test

--- 91-cdn/data.tf
+++ 91-cdn/data.tf
@@ -1,6 +1,6 @@
 
 data "aws_ssm_parameter" "acm_certificate_arn" {
-  name = "/${var.project}/${var.environment}/acm_certificate_arn"
+  name = "/${var.project}-${var.environment}/acm_certificate_arn"
 }
 data "aws_cloudfront_cache_policy" "cacheEnable" {
   name = "Managed-CachingOptimized" 
